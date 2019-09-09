0c2ca8fb7bc3a14200b9b14485353669cac2bad0
infotodoo <info@todoo.co>
2019-09-09 16:03:28 -0500

    new change

--- cliente-todoo-master/clientes/todoo_partner_customization/models/partner.py
+++ cliente-todoo-master/clientes/todoo_partner_customization/models/partner.py
@@ -60,6 +60,7 @@ class ResPartner(models.Model):
     name = fields.Char(default=lambda self: self.first_name)
     first_name = fields.Char()
     middle_name = fields.Char()
+    pater=field.char()
     last_name = fields.Char()
     second_last_name = fields.Char()
     nit = fields.Char(string='NIT', size=9)
